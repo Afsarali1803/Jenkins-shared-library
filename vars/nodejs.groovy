7f6c6edf0527b4b184f084dcb05435238b9c83e9
= <=>
2022-11-27 12:00:33 +0530

    test

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,20 +1,20 @@
-def lintChecks(COMPONENTS) {
+def lintChecks(COMPONENT) {
         sh "echo installing jslinst"
         sh "npm i jslint"   
         sh "node_modules/jslint/bin/jslint.js server.js || true"
-        sh "echo Lint Checks Completed ($COMPONENTS) "
+        sh "echo Lint Checks Completed ($COMPONENT) "
 }
 
 // call is the function which will be called by default.
 // function call will be called by default, when you call the fileName
-def call(COMPONENTS) {
+def call(COMPONENT) {
     pipeline{
         agent any
         stages {
             stage('Lint Checks') {
                 steps {
                     script {
-                        lintChecks(COMPONENTS)                  // Use script { when you're using groovy based conventions }
+                        lintChecks(COMPONENT)                  // Use script { when you're using groovy based conventions }
                     }
                 }
             }   
