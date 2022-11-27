9bef14ba4d591e06b7a1115291461dd43e92446c
= <=>
2022-11-27 11:23:09 +0530

    common file

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,7 +2,7 @@ def lintChecks() {
         sh "echo installing jslinst"
         sh "npm i jslint"   
         sh "node_modules/jslint/bin/jslint.js server.js || true"
-        sh "echo Lint Checks Completed for $COMPONENT"
+        sh "echo Lint Checks Completed for "
 }
 
 // call is the function which will be called by default.
