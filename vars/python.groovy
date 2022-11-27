34f04dfb6ead36a86457272fb5d2a40d78009204
= <=>
2022-11-27 13:23:31 +0530

    test

---
+++ vars/python.groovy
@@ -0,0 +1,28 @@
+def lintChecks() {
+    sh "echo list checks started for payment * * * * ......"
+    sh "echo Lint Checks Completed for $COMPONENT"
+}
+
+// function call will be called by default, when you call the fileName
+def call() {
+    pipeline{ 
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintChecks()                  // Use script { when you're using groovy based conventions }
+                    }
+                }
+            }
+
+            stage('Sonar Checks') {
+                steps {
+                    script {
+                        common.sonarChecks()                  // Use script { when you're using groovy based conventions }
+                    }
+                }
+            }
+
+        }   // end of stages 
+    }  // end of pipelines
+} // end of call
