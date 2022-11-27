e36201bdb5562577ac3d5a8adbe8d41b56bfbae7
= <=>
2022-11-27 10:45:51 +0530

    jenkins-shared

---
+++ vars/nodejs.groovy
@@ -0,0 +1,6 @@
+def lintChecks() {
+        sh "echo installing jslinst"
+        sh "npm i jslint"   
+        sh "node_modules/jslint/bin/jslint.js server.js || true"
+        sh "echo Lint Checks Completed for $COMPONENT"
+}
---
+++ vars/sample.groovy
@@ -0,0 +1,6 @@
+
+def info(message, URL) {
+    echo "Info: ${message} and the url is ${URL}"
+}
+
+info("Devops","www.google.com")
