f71b23fbcac2fc239d3ca12098b78825a16d233f
= <=>
2022-11-30 23:36:38 +0530

    test

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,12 +4,15 @@ def lintChecks() {
         sh "node_modules/jslint/bin/jslint.js server.js || true"
         sh "echo Lint Checks Completed $COMPONENT"
 }
-
 // call is the function which will be called by default.
 // function call will be called by default, when you call the fileName
 def call() {
     pipeline{
         agent any
+        environment {
+            SONAR    = credentials('SONAR')
+            SONARURL = "172.31.10.167"
+        }
         stages {
             stage('Lint Checks') {
                 steps {
@@ -18,10 +21,12 @@ def call() {
                     }
                 }
             }   
-            stage('XYZ Checks') {
+            stage('Sonar Checks') {
                 steps {
-                    sh "echo Performing XYZ Checks"
-                    sh "echo this takes 30 mins"
+                    script {
+                        env.ARGS="-Dsonar.sources=."
+                        common.sonarChecks()                  // Use script { when you're using groovy based conventions }
+                    }
                 }
             } 
 
