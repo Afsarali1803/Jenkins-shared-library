f4dd12a03c7a4a1a8facdb9595cd25686f13ab27
= <=>
2022-12-03 22:23:46 +0530

    test

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,33 +1,44 @@
 def sonarChecks() {
-        stage('sonarcheck')
+        stage('sonarcheck') {
                 sh "echo Starting Code Quality Analysis"
                 //sh "sonar-scanner -Dsonar.host.url=http://172.31.10.167:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} ${ARGS}"
                 //sh "sonar-scanner -Dsonar.host.url=http://172.31.10.167:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} ${ARGS}"
                 //sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONARURL} ${COMPONENT}" 
                 sh "echo Code Quality Analysis is Completed"
+        }
 }
 
 def lintChecks() {
-     stage('Lint check')   
-        if(env.APPTYPE == "nodejs") {   
-                sh "echo installing jslinst"
-                sh "npm i jslint"   
-                sh "node_modules/jslint/bin/jslint.js server.js || true"
-                sh "echo Lint Checks Completed $COMPONENT"
-        }
-        else if(env.APPTYPE == "maven") {
-                sh "mvn checkstyle:check || true"
-                sh "echo Lint check completed for $COMPONENT"        
-        }
-        else if(env.APPTYPE == "python") {
-                sh "echo list checks started for payment * * * * ......"
-                sh "echo Lint check completed for $COMPONENT"        
-        }
-        else if(env.APPTYPE == "golang") {
-                sh "echo list checks started for golang * * * * ......"
-                sh "echo Lint check completed for $COMPONENT"        
-        }
-        else
-                sh "echo doing generic lint check
+        stage('Lint check')  {
+                if(env.APPTYPE == "nodejs") {   
+                        sh "echo installing jslinst"
+                        sh "npm i jslint"   
+                        sh "node_modules/jslint/bin/jslint.js server.js || true"
+                        sh "echo Lint Checks Completed $COMPONENT"
+                }
+                else if(env.APPTYPE == "maven") {
+                        sh "mvn checkstyle:check || true"
+                        sh "echo Lint check completed for $COMPONENT"        
+                }
+                else if(env.APPTYPE == "python") {
+                        sh "echo list checks started for payment * * * * ......"
+                        sh "echo Lint check completed for $COMPONENT"        
+                }
+                else if(env.APPTYPE == "golang") {
+                        sh "echo list checks started for golang * * * * ......"
+                        sh "echo Lint check completed for $COMPONENT"        
+                }
+                else
+                        sh "echo doing generic lint check"
+        
         }
 }
+
+def testcases() {
+        stage ('test case')
+                sh "echo Unit Test"
+}
+        
+
+
+
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,93 +1,105 @@
-def lintChecks() {
-        sh "echo installing jslinst"
-        sh "npm i jslint"   
-        sh "node_modules/jslint/bin/jslint.js server.js || true"
-        sh "echo Lint Checks Completed $COMPONENT"
+env.APPTYPE = "nodejs"
+def call () {
+    node {
+        common.lintChecks ()
+        env.ARGS="-Dsonar.sources=."
+        common.sonarcheck ()
+        common.testcases ()
+    }
+
 }
-// call is the function which will be called by default.
-// function call will be called by default, when you call the fileName
-def call() {
-    pipeline{
-        agent any
-        environment {
-            SONAR    = credentials('SONAR')
-            NEXUS   = credentials('NEXUS')
-            SONARURL = "172.31.10.167"
-            NEXUSURL = "172.31.4.161"
-        }
-        stages {
-            stage('Lint Checks') {
-                steps {
-                    script {
-                        lintChecks()                  // Use script { when you're using groovy based conventions }
-                    }
-                }
-            }   
-            // stage('Sonar Checks') {
-            //     steps {
-            //         script {
-            //             env.ARGS="-Dsonar.sources=."
-            //             common.sonarChecks()                  // Use script { when you're using groovy based conventions }
-            //         }
-            //     }
-            // } 
 
-            stage('Test Cases') {
-                parallel {
-                    stage('Unit Testing') {                 
-                        steps {
-                            sh "echo Unit Testing Completed"   
-                                }
-                            }
-                    stage('Integration Testing') {                 
-                        steps {
-                            sh "echo Integration Testing Completed"   
-                                }
-                            }
-                    stage('Function Testing') {                 
-                        steps {
-                            sh "echo Function Testing Completed"   
-                                }
-                            }
-                        }         
-                    }
 
-            stage('Chekcing Artifacts') {
-                when { expression { env.TAG_NAME != null } }
-                steps {
-                    script {
-                        env.UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -s http://${NEXUSURL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip ||  true' )
-                        print UPLOAD_STATUS
-                      }
-                  }
-             }
+// def lintChecks() {
+//         sh "echo installing jslinst"
+//         sh "npm i jslint"   
+//         sh "node_modules/jslint/bin/jslint.js server.js || true"
+//         sh "echo Lint Checks Completed $COMPONENT"
+// }
+// // call is the function which will be called by default.
+// // function call will be called by default, when you call the fileName
+// def call() {
+//     pipeline{
+//         agent any
+//         environment {
+//             SONAR    = credentials('SONAR')
+//             NEXUS   = credentials('NEXUS')
+//             SONARURL = "172.31.10.167"
+//             NEXUSURL = "172.31.4.161"
+//         }
+//         stages {
+//             stage('Lint Checks') {
+//                 steps {
+//                     script {
+//                         lintChecks()                  // Use script { when you're using groovy based conventions }
+//                     }
+//                 }
+//             }   
+//             // stage('Sonar Checks') {
+//             //     steps {
+//             //         script {
+//             //             env.ARGS="-Dsonar.sources=."
+//             //             common.sonarChecks()                  // Use script { when you're using groovy based conventions }
+//             //         }
+//             //     }
+//             // } 
 
-            stage('Prepare Artifacts') {
-                when { expression { env.TAG_NAME != null } 
-                       expression { env.UPLOAD_STATUS == "" } }
-                steps {
-                    sh "npm install"
-                    sh "zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js"
-                    sh "echo Prepare Artifacts done"
-                }
-            }
+//             stage('Test Cases') {
+//                 parallel {
+//                     stage('Unit Testing') {                 
+//                         steps {
+//                             sh "echo Unit Testing Completed"   
+//                                 }
+//                             }
+//                     stage('Integration Testing') {                 
+//                         steps {
+//                             sh "echo Integration Testing Completed"   
+//                                 }
+//                             }
+//                     stage('Function Testing') {                 
+//                         steps {
+//                             sh "echo Function Testing Completed"   
+//                                 }
+//                             }
+//                         }         
+//                     }
 
-            stage('Uploading Artifacts') {
-                when { expression { env.TAG_NAME != null } 
-                       expression { env.UPLOAD_STATUS == "" } }
-                steps {
-                    sh "echo Uploading started."
-                    sh "echo ${TAG_NAME}"
-                    sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUSURL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
-                    }
-               }
+//             stage('Chekcing Artifacts') {
+//                 when { expression { env.TAG_NAME != null } }
+//                 steps {
+//                     script {
+//                         env.UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -s http://${NEXUSURL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip ||  true' )
+//                         print UPLOAD_STATUS
+//                       }
+//                   }
+//              }
 
-            stage('ABC Checks') {
-                steps {
-                    sh "echo Performing XYZ Checks"
-                    sh "echo this takes 30 mins"
-                }
-            } 
-        }   // end of stages 
-    }  // end of pipelines
-}//end of call
+//             stage('Prepare Artifacts') {
+//                 when { expression { env.TAG_NAME != null } 
+//                        expression { env.UPLOAD_STATUS == "" } }
+//                 steps {
+//                     sh "npm install"
+//                     sh "zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js"
+//                     sh "echo Prepare Artifacts done"
+//                 }
+//             }
+
+//             stage('Uploading Artifacts') {
+//                 when { expression { env.TAG_NAME != null } 
+//                        expression { env.UPLOAD_STATUS == "" } }
+//                 steps {
+//                     sh "echo Uploading started."
+//                     sh "echo ${TAG_NAME}"
+//                     sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUSURL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+//                     }
+//                }
+
+//             stage('ABC Checks') {
+//                 steps {
+//                     sh "echo Performing XYZ Checks"
+//                     sh "echo this takes 30 mins"
+//                 }
+//             } 
+//         }   // end of stages 
+//     }  // end of pipelines
+// }//end of call
